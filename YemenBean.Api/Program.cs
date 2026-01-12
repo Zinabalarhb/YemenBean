cbb071ddfe870ea5737ac237c6dafa007cd52792
zinab <zinabalarhb@gmail.com>
2026-01-12 03:14:18 +0300

    v2

--- YemenBean.Api/Program.cs
+++ YemenBean.Api/Program.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using YemenBean.Core.Features.Products.Queries.Handlers;
 using YemenBean.Infrastructure.Context;
@@ -12,7 +13,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+//builder.Services.AddOpenApi();
 
 
 
@@ -21,7 +22,7 @@ builder.Services.AddOpenApi();
 /// =======================
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-//builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen();
 
 /// =======================
 /// 2️⃣ Database
@@ -52,12 +53,16 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
+
+
 app.UseHttpsRedirection();
-
+app.UseStaticFiles();
 app.UseAuthorization();
-
+app.UseCors("AllowAll");
 app.MapControllers();
 
 app.Run();
